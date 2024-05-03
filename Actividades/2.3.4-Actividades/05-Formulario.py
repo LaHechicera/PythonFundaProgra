f444b5e466ee157310b38fccb9464126cbee0df5
LaHechicera <ca.gonzalezz@duocuc.cl>
2024-05-02 21:44:34 -0400

    comiteo

--- Actividades/2.3.4-Actividades/05-Formulario.py
+++ Actividades/2.3.4-Actividades/05-Formulario.py
@@ -9,15 +9,19 @@ print("B. Cassandra Cain")
 print("C. Stephaine Brown")
 print("D. Harley Quinn")
 print(" ")
-respt = input("Ingrese su respuesta (Ej: D) ")
+respt1 = input("Ingrese su respuesta (Ej: D) ")
 
-if respt == "A":
-    print("Respuesta correcta, ganas 1.0 punto")
-elif respt == "B":
-    print("A pesar de que si fue una Batgirl no es la original, ganas 0.5 puntos")
-elif respt == "C":
-    print("A pesar de que si fue una Batgirl no es la original, ganas 0.2 puntos")
+if respt1 == "A":
+    respt1 = 5
+    print("Respuesta correcta, ganas 5 punto")
+elif respt1 == "B":
+    respt1 = 3
+    print("A pesar de que si fue una Batgirl no es la original, ganas 3 puntos")
+elif respt1 == "C":
+    respt1 = 1
+    print("A pesar de que si fue una Batgirl no es la original, ganas 1 puntos")
 else:
+    respt1 = 0
     print("Harley Quinn es una antiheroe, ganas 0 puntos")
 
 print(" ")
@@ -28,16 +32,20 @@ print("B. Jim Gordon")
 print("C. Joker")
 print("D. Gatubela")
 print(" ")
-respt = input("Ingrese su respuesta ")
+respt2 = input("Ingrese su respuesta ")
 
-if respt == "A":
-    print("A pesar de que si es un villano no es el principal, ganas 0.5 punto")
-elif respt == "B":
+if respt2 == "A":
+    respt2 = 3
+    print("A pesar de que si es un villano no es el principal, ganas 3 punto")
+elif respt2 == "B":
+    respt2 = 0 
     print("Jim Gordon es el comisionado de policia, ganas 0 puntos")
-elif respt == "C":
-    print("Respuesta correcta, ganas 1.0 puntos")
+elif respt2 == "C":
+    respt2 = 5
+    print("Respuesta correcta, ganas 5 puntos")
 else:
-    print("Gatubela es una antiheroe, ganas 0.2 puntos")
+    respt2 = 1
+    print("Gatubela es una antiheroe, ganas 1 puntos")
 
 print(" ")
 print("¿Que Robin murio a manos del Joker en el primer comic \"La Muerte en la Familia\"?")
@@ -47,14 +55,21 @@ print("B. Stephanie Brown")
 print("C. Victor Szas")
 print("D. Jason Todd")
 print(" ")
-respt = input("Ingrese su respuesta ")
+respt3 = input("Ingrese su respuesta ")
 
-if respt == "A":
-    print("Dick Grayson fue el primer Robin pero no murio a manos del Joker, ganas 0.5 punto")
-elif respt == "B":
+if respt3 == "A":
+    respt3 = 3
+    print("Dick Grayson fue el primer Robin pero no murio a manos del Joker, ganas 3 punto")
+elif respt3 == "B":
+    respt3 = 0
     print("A pesar de que si fue un Robin por más tiempo fue una Batgirl, ganas 0 puntos")
-elif respt == "C":
-    print("Es un villano, ganas 0.2 puntos")
+elif respt3 == "C":
+    respt3  = 1
+    print("Es un villano, ganas 1 puntos")
 else:
-    print("Respuesta correcta, ganas 1.0 puntos")
+    respt3 = 5
+    print("Respuesta correcta, ganas 5 puntos")
 
+puntos = respt1+respt2+respt3
+
+print("Su puntaje es ", puntos, " de 15")
