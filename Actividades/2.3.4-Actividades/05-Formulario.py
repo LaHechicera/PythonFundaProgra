7ec6fb497ad0b05f9e074293cf38a8e924b4d28b
LaHechicera <ca.gonzalezz@duocuc.cl>
2024-05-02 22:10:06 -0400

    12

--- Actividades/2.3.4-Actividades/05-Formulario.py
+++ Actividades/2.3.4-Actividades/05-Formulario.py
@@ -4,6 +4,8 @@ print("A continuación deberas responder unas preguntas referentes a \n\"DC comi
 print(" ")
 print("¿Cuál es la identidad secreta de Batgirl original?")
 print(" ")
+import time
+time.sleep (5)
 print("A. Barbara Gordon")
 print("B. Cassandra Cain")
 print("C. Stephaine Brown")
@@ -13,16 +15,18 @@ respt1 = input("Ingrese su respuesta (Ej: D) ")
 
 if respt1 == "A":
     respt1 = 5
-    print("Respuesta correcta, ganas 5 punto")
+    print("Respuesta correcta")
 elif respt1 == "B":
     respt1 = 3
-    print("A pesar de que si fue una Batgirl no es la original, ganas 3 puntos")
+    print("A pesar de que si fue una Batgirl no es la original")
 elif respt1 == "C":
     respt1 = 1
-    print("A pesar de que si fue una Batgirl no es la original, ganas 1 puntos")
+    print("A pesar de que si fue una Batgirl no es la original")
 else:
     respt1 = 0
-    print("Harley Quinn es una antiheroe, ganas 0 puntos")
+    print("Harley Quinn es una antiheroe")
+
+
 
 print(" ")
 print("¿Cuál es el villano principal de Batman?")
@@ -36,16 +40,16 @@ respt2 = input("Ingrese su respuesta ")
 
 if respt2 == "A":
     respt2 = 3
-    print("A pesar de que si es un villano no es el principal, ganas 3 punto")
+    print("A pesar de que si es un villano no es el principal")
 elif respt2 == "B":
     respt2 = 0 
-    print("Jim Gordon es el comisionado de policia, ganas 0 puntos")
+    print("Jim Gordon es el comisionado de policia")
 elif respt2 == "C":
     respt2 = 5
-    print("Respuesta correcta, ganas 5 puntos")
+    print("Respuesta correcta")
 else:
     respt2 = 1
-    print("Gatubela es una antiheroe, ganas 1 puntos")
+    print("Gatubela es una antiheroe")
 
 print(" ")
 print("¿Que Robin murio a manos del Joker en el primer comic \"La Muerte en la Familia\"?")
@@ -59,16 +63,16 @@ respt3 = input("Ingrese su respuesta ")
 
 if respt3 == "A":
     respt3 = 3
-    print("Dick Grayson fue el primer Robin pero no murio a manos del Joker, ganas 3 punto")
+    print("Dick Grayson fue el primer Robin pero no murio a manos del Joker")
 elif respt3 == "B":
     respt3 = 0
-    print("A pesar de que si fue un Robin por más tiempo fue una Batgirl, ganas 0 puntos")
+    print("A pesar de que si fue un Robin por más tiempo fue una Batgirl")
 elif respt3 == "C":
     respt3  = 1
-    print("Es un villano, ganas 1 puntos")
+    print("Es un villano")
 else:
     respt3 = 5
-    print("Respuesta correcta, ganas 5 puntos")
+    print("Respuesta correcta")
 
 puntos = respt1+respt2+respt3
 
@@ -78,3 +82,6 @@ elif puntos > 6 and puntos < 10:
     print("Su puntaje es ", puntos, " de 15, no sabe mucho de DC comics")
 else:
     print("Su puntaje es ", puntos, " de 15 usted sabe mucho de DC comics")
+
+#import time
+#time.sleep(segundos) => temporizador, cuenta segundos
