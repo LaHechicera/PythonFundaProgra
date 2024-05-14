2001bac63179a026dc226ba508eb1a030f0788c3
LaHechicera <ca.gonzalezz@duocuc.cl>
2024-05-14 19:28:56 -0400

    jkajk

--- Problema.py
+++ Problema.py
@@ -20,7 +20,7 @@ while True:
 
         total = panCiabcosto+pieLimcosto+cafecosto+tecosto+alfajorcosto
         break
-    except ValueError:
+    except ValueError and Exception:
         print("Favor ingrese un número valido")
 
     finally:
@@ -29,11 +29,11 @@ while True:
 print(" ")
 print("-----Resumen de ventas-----")
 print(" ")
-print(f"Total Pan Ciabata: {panCiabcosto}")
-print(f"Total Pie de Limón: {pieLimcosto}")
-print(f"Total Cafe: {cafecosto}")
-print(f"Total té: {tecosto}")
-print(f"Total Alfajores: {alfajorcosto}")
+print(f"Total Pan Ciabata (Vendidos {panCiab}): {panCiabcosto}")
+print(f"Total Pie de Limón (Vendidos {pieLim}): {pieLimcosto}")
+print(f"Total Cafe (Vendidos {cafe}): {cafecosto}")
+print(f"Total té (Vendidos {te}): {tecosto}")
+print(f"Total Alfajores (Vendidos {alfajor}): {alfajorcosto}")
 print(f"Total ventas: {total}\n")
 
 if total >= 50000:
@@ -45,9 +45,9 @@ else:
 
 with open('Informe_ventas.txt', 'w') as archivo:
     archivo.write("\n-----Resumen de ventas-----")
-    archivo.write(f"\nTotal Pan Ciabatta: {panCiabcosto}")
-    archivo.write(f"\nTotal Pie de Limon: {pieLimcosto}")
-    archivo.write(f"\nTotal Cafe: {cafecosto}")
-    archivo.write(f"\nTotal te: {tecosto}")
-    archivo.write(f"\nTotal Alfajores: {alfajorcosto}")
+    archivo.write(f"\nTotal Pan Ciabatta (Vendidos {panCiab}): {panCiabcosto}")
+    archivo.write(f"\nTotal Pie de Limon (Vendidos {pieLim}): {pieLimcosto}")
+    archivo.write(f"\nTotal Cafe (Vendidos {cafe}): {cafecosto}")
+    archivo.write(f"\nTotal te (Vendidos {te}): {tecosto}")
+    archivo.write(f"\nTotal Alfajores (Vendidos {alfajor}): {alfajorcosto}")
     archivo.write(f"\nTotal ventas: {total}")
